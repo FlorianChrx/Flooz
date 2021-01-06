12a38e71929e70adb8a3c325cc8dbf4c49ab336d
FlorianChrx <florianchiro@hotmail.fr>
2021-01-06 09:21:04 +0100

    mini refactoring

--- src/main/java/com/owle/OwleAPI/model/Group.java
+++ src/main/java/com/owle/OwleAPI/model/Group.java
@@ -1,11 +1,19 @@
 package com.owle.OwleAPI.model;
 
 import com.owle.OwleAPI.ids.GroupId;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
 @IdClass(GroupId.class)
 public class Group {
@@ -22,39 +30,5 @@ public class Group {
     }
 
     public Group() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Group group = (Group) o;
-
-        if (getId() != group.getId()) return false;
-        return getUser().equals(group.getUser());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getId();
-        result = 31 * result + getUser().hashCode();
-        return result;
     }
 }
