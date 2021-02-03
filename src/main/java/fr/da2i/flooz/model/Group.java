1a942d0a6b87671781d0c4ec7586af8d6d6fdf95
FlorianChrx <43887259+FlorianChrx@users.noreply.github.com>
2021-02-03 11:49:16 +0100

    change model

--- src/main/java/fr/da2i/flooz/model/Group.java
+++ src/main/java/fr/da2i/flooz/model/Group.java
@@ -1,25 +1,22 @@
 package fr.da2i.flooz.model;
 
-import fr.da2i.flooz.ids.GroupId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@IdClass(GroupId.class)
 @Table(name = "groups")
 public class Group {
     @Id
     public int id;
-
     @Id
-    public User user;
+    @OneToMany
+    public List<User> users;
+    private String name;
 }
--- src/main/java/fr/da2i/flooz/model/Transaction.java
+++ src/main/java/fr/da2i/flooz/model/Transaction.java
@@ -1,6 +1,5 @@
 package fr.da2i.flooz.model;
 
-import fr.da2i.flooz.ids.GroupId;
 import fr.da2i.flooz.ids.TransactionId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.OneToOne;
 
 @Data
 @AllArgsConstructor
@@ -17,8 +17,10 @@ import javax.persistence.IdClass;
 @IdClass(TransactionId.class)
 public class Transaction {
     @Id
-    private GroupId tgroup;
+    @OneToOne
+    private Group group;
     @Id
+    @OneToOne
     private User spender;
     private Double amount;
 }
