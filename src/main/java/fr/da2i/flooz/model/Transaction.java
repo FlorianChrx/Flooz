0ee1d057333b6e1c751281848bd57c17a19419be
FlorianChrx <43887259+FlorianChrx@users.noreply.github.com>
2021-03-27 13:31:03 +0100

    big bug (#11)
    * unknown commit
    
    * fix big problems
    
    * fix bug

--- src/main/java/fr/da2i/flooz/model/Transaction.java
+++ src/main/java/fr/da2i/flooz/model/Transaction.java
@@ -1,11 +1,9 @@
 package fr.da2i.flooz.model;
 
-import fr.da2i.flooz.ids.TransactionId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -16,13 +14,9 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "transactions")
 public class Transaction {
-	@Id
+    @Id
     private Integer id;
     private Double amount;
-    
     private Group tgroup;
-    
     private User user;
-    
-    
 }
